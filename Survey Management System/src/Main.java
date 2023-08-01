c765b0b4ce3c6f14fbf7ddd6200d74a08d4c7b52
daredaryl96 <113288805+daredaryl96@users.noreply.github.com>
2023-08-01 17:29:13 +0800

    Added code for Guest.java and SQLManage.java

--- Survey Management System/src/Main.java
+++ Survey Management System/src/Main.java
@@ -2,7 +2,7 @@
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String args[]) throws SQLException {
+    public static void Main(String args[]) throws SQLException {
         Login login = new Login();
         login.loginView();
     }
